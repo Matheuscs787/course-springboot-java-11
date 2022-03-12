75f4b64b6f6ece9eaf8c81c379998be4ba121173
Matheus787 <matheus787@hotmail.com>
2022-03-12 10:39:51 -0300

    Service layer, component registration

--- src/main/java/com/matheus787/course/repositories/UserRepository.java
+++ src/main/java/com/matheus787/course/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.matheus787.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matheus787.course.entities.User;
-
+ 
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
